309788b0cc924302e8837e9c4ee79a2de011fbb8
Dylan <dxa4481@rit.edu>
2013-10-14 03:27:18 -0400

    adding volume set

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express')
     , http = require('http')
     , path = require('path');
+var util = require('util'),
+    exec = require('child_process').exec,
+    child;
 
 var hatActions = require('./routes/hatActions');
 var app = express();
@@ -23,4 +26,13 @@ app.post('/', hatActions.home_post_handler);
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
+    child = exec('amixer set PCM -- 1000', // command line argument directly in string
+        function (error, stdout, stderr) {      // one easy function to capture data/errors
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
+
 });
